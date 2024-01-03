a5a571237f2fddae9a6eb6decb97757b9580edd7
aisiwonhae29 <aisiwonhae29@gmail.com>
2024-01-04 08:18:46 +0900

    until 240103

--- daily_pr/24/1/02/00promise.js
+++ daily_pr/24/1/02/00promise.js
@@ -4,12 +4,21 @@ function doSomething() {
         // Other things to do before completion of the promise
         console.log("Did something");
         // The fulfillment value of the promise
-        // resolve("https://example.com/");
-        throw new Error('error!!s');
+        resolve("https://example.com/");
+        // throw new Error('error!!s');
       }, 200);
     });
   }
 
+  function test1(result1){
+    console.log(123)
+    return result1;
+  }
+
+  function test2(result2){
+    console.log(44444);
+    return result2;
+  }
   const promise = doSomething();
-  const promise2 = promise.then((result)=>{console.log(result)}, (err)=>{console.error(err)});
+  const promise2 = promise.then((result)=>{console.log(result)}, (err)=>{console.error(err)}).then((result1)=>test1(result1)).then((result2)=>test2(result2));
   
